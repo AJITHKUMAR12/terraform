718eff0ab87168655eb4d638b202d7c9a727d0a8
AJITHKUMAR12 <48044092+AJITHKUMAR12@users.noreply.github.com>
2024-08-09 20:04:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "google_storage_bucket" "jenkins" {
   project = "moonlit-grail-428807-k4"
   location = "us-central1"
 }
+resource "google_storage_bucket" "jenkins-github" {
+  name     = "gcp_jenkins_github"
+  project = "moonlit-grail-428807-k4"
+  location = "us-central1"
+}
